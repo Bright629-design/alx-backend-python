1207616d653a82e7563c31f90106cd0eaf0a8456
BrightDesign <cheruiyotemicky@gmail.com>
2025-07-22 10:40:30 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,3 +4,14 @@ def access_nested_map(nested_map, path):
     for key in path:
         nested_map = nested_map[key]
     return nested_map
+def memoize(method):
+    """Decorator to cache method output."""
+    attr_name = "_{}".format(method.__name__)
+
+    @property
+    def wrapper(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, method(self))
+        return getattr(self, attr_name)
+    
+    return wrapper
